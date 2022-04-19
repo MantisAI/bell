a8c1ddd1adcd79c675d7e4e6a0daaa169f605719
Nick Sorros <nsorros@gmail.com>
2022-04-19 11:53:51 +0300

    Add command and test

--- bell.py
+++ bell.py
@@ -1,14 +1,18 @@
+import subprocess
+import argparse
+import os
+
 from loguru import logger
 import requests
-import typer
 import boto3
 
 
-app = typer.Typer()
+BELL_MODE = os.environ["BELL_MODE"]
 
+def get_status_message():
+    if BELL_MODE == "LOCAL":
+        return ":bellhop_bell: local mode"
 
-@app.command()
-def bell(webhook_url):
     response = requests.get("http://169.254.169.254/latest/meta-data/instance-id")
     instance_id = response.text
 
@@ -24,12 +28,48 @@ def bell(webhook_url):
     response = ec2.describe_instance_status(InstanceIds=[instance_id])
     status = response["InstanceStatuses"][0]["InstanceState"]["Name"]
     logger.debug(f"ec2 instance status: {status}")
+    return f":bellhop_bell: {ec2_instance_name} is {status}"
+
+
+def send_slack_message(message):
+    logger.debug(f"slack message: {message}")
+
+    if BELL_MODE == "LOCAL":
+        return
 
     response = requests.post(
-        webhook_url, json={"text": f":bellhop_bell: {ec2_instance_name} is {status}"}
+        webhook_url, json={"text": message}
     )
     logger.debug(f"slack response code: {response.status_code}")
 
 
+def bell(webhook_url, *command_args):
+    if command_args:
+        logger.debug(f"command: {command_args}")
+        try:
+            send_slack_message("command started")
+            subprocess.run(command_args, check=True)
+        except subprocess.CalledProcessError:
+            logger.error("command failed")
+            send_slack_message("command failed")
+            raise
+
+        send_slack_message("command finished")
+    else:
+        instance_status = get_status_message()
+        send_slack_message(instance_status)
+
+
+def cli():
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("--webhook-url", help="incoming slack webhook url")
+
+    args, command_args = argument_parser.parse_known_args()
+    logger.debug(f"args: {args}")
+    logger.debug(f"command_args: {command_args}")
+
+    bell(args.webhook_url, *command_args)
+
+
 if __name__ == "__main__":
-    app()
+    cli()
---
+++ tests/test_bell.py
@@ -0,0 +1,18 @@
+import subprocess
+
+import pytest
+
+from bell import bell
+
+
+def test_bell():
+    bell("slack webhook url")
+
+def test_bell_command():
+    command_args = "python -c \"print('hello')\"".split()
+    bell("slack webhook url", *command_args)
+
+def test_bell_bad_command():
+    command_args = """ python -c "print('hello' """.split()
+    with pytest.raises(subprocess.CalledProcessError):
+        bell("slack webhook url", *command_args)
