7a1f20d5a5921d2a61abce4b9002632ca7b2752d
Nick Sorros <nsorros@gmail.com>
2022-04-29 10:07:58 +0300

    Remove loguru and add boto as extra

--- bell.py
+++ bell.py
@@ -1,13 +1,16 @@
 from socket import timeout
 import urllib.request
 import subprocess
+import logging
 import argparse
 import json
 import sys
 import os
 
-from loguru import logger
-import boto3
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(funcName)20s:%(lineno)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 def get_status_message():
@@ -16,11 +19,21 @@ def get_status_message():
             "http://169.254.169.254/latest/meta-data/instance-id", timeout=5
         )
         instance_id = response.read()
+
+        response = urllib.request.urlopen(
+            "http://169.254.169.254/latest/meta-data/public-hostname", timeout=5
+        )
+        public_hostname = response.read()
     except urllib.request.URLError:
         logger.debug(
             "Metadata request timed out. You are probably not in an AWS instance."
         )
         return ":bellhop_bell: local mode"
+
+    try:
+        import boto3
+    except ImportError:
+        return f":bellhop_bell: {public_hostname} is running"
 
     ec2 = boto3.client("ec2")
     response = ec2.describe_tags(
